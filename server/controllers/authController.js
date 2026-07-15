9cdbd45181b36c0871f22d0e10c0bce9e6a5a1ed
Gautamikahar <gautami.kahar@gmail.com>
2026-07-15 14:58:57 +0530

    Added SMTP verification

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -15,12 +15,13 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // use TLS
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+
 transporter.verify((error, success) => {
   if (error) {
     console.log("SMTP ERROR:", error);
